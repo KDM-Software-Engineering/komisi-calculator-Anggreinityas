14ef64ab72187302af9a5ceb39818f8dcf1297b0
Anggreinityas <anggreinityas12@gmail.com>
2024-10-30 10:47:54 +0700

    selesai

---
+++ appp.js
@@ -0,0 +1,5 @@
+let tinggiBadan = ["150, 155, 148, 165, 170, 168, 166"]
+let namaAtlet = ["Rizky", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
+for (let i = 0; i < 7 ; i++) {
+    console.log(`Atlet ${namaAtlet[i]} tinggi badanya ${tinggiBadan [i]} cm`)
+}
